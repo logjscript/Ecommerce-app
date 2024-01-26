273844fc696a904030eec1e5a88c5de87007ed55
Logan Dietel <logandietel1@gmail.com>
2024-01-26 01:49:49 -0800

    Add quantity method to each item and fix Bag error
    The price method for each item was changed to value for consistency.
    
    Ref was added in Button to update quantity properly.

--- client/src/components/Bag.jsx
+++ client/src/components/Bag.jsx
@@ -2,19 +2,34 @@
 
 export default function Bag({ userInfo }) {
 
+    const userTotalPrice = userInfo.items?.reduce((acc, item) => {    
+        if (item.value[0] === '$') {
+            return acc + Number(item.value.slice(1));
+        } else {
+            return acc + Number(item.value);
+        }
+    }, 0);
+    console.log(userTotalPrice)
+
     return (
-        userInfo.items ? (
-            <div className="pt-[8rem]">
-                {userInfo.items.map((item => (
-                    <div key={item.item_name} className="flex gap-4 justify-center">
-                        <img src={item.img_path} className="w-[200px]"/>
-                        <div>{item.item_name}</div>
-                        <div>{item.price}</div>
+        <>
+            <h1 className="pt-[8rem]">Total: {userTotalPrice}</h1>
+            <div>
+                {userInfo.items ? (
+                    <div className="pt-[8rem]">
+                        {userInfo.items.map((item => (
+                            <div key={item.name} className="flex gap-4 justify-center">
+                                <img src={item.link} className="w-[200px]"/>
+                                <div>{item.name}</div>
+                                <div>{item.value}</div>
+                            </div>
+                        )))}
                     </div>
-                )))}
+                ) : (
+                    <div className="pt-[8rem] text-center">No Items</div>
+                )}
             </div>
-        ) : (
-            <div className="pt-[8rem] text-center">No Items</div>
-        )
+        </>
+        
     )
 }
--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,42 +1,82 @@
+import { useEffect, useRef } from "react";
 
 export default function Button({ item, userInfo, setUserInfo, signedIn, setCanceled }) {
-
-    const itemToBag = async () => {
-        try {
-            const response = await fetch(`http://localhost:5200/api/v1/users/${userInfo.username}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({...userInfo, items: [
-                    ...userInfo.items, {price: item.value, img_path: item.link, item_name: item.name}
-                ]})
-            });
-
-            if (!response.ok) {
-                throw new Error('Data cannot be sent');
-            }
-        } catch (error) {
-            console.error(error);
+    const quantityRef = useRef(null);
+    
+    useEffect(() => {
+        const existingItem = userInfo.items?.find(oldItem => oldItem.name === item.name);
+        if (existingItem) {
+            quantityRef.current = existingItem.quantity;
+        } else {
+            quantityRef.current = 0;
         }
-    }
+        
+        const itemToBag = async () => {
+            try {
+                const response = await fetch(`http://localhost:5200/api/v1/users/${userInfo.username}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({...userInfo, items: [
+                        ...(userInfo.items || []), {value: item.value, link: item.link, name: item.name, quantity: quantityRef.current}
+                    ]})
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Data cannot be sent');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        if (userInfo.items) {
+            itemToBag();
+        }
+    }, [userInfo.items])
 
     const handleClick = async () => {
-        !signedIn ? setCanceled(false) : await itemToBag();
-        setUserInfo({
-            ...userInfo, items: [
-                ...(userInfo.items || []), {price: item.value, img_path: item.link, item_name: item.name}
-            ]
-        })
+        try {
+            if (signedIn) {
+                const existingItemIndex = userInfo.items?.findIndex(bagItem => {
+                    console.log(bagItem.name);
+                    console.log(item.name);
+                    return bagItem.name === item.name;
+                });
+                console.log(existingItemIndex);
+
+                if (existingItemIndex !== -1 && existingItemIndex !== undefined) {
+                    let updatedState = [...userInfo.items];
+                    updatedState[existingItemIndex] = {
+                        ...item, 
+                        quantity: userInfo.items[existingItemIndex].quantity + 1
+                    };
+
+                    setUserInfo({
+                        ...userInfo,
+                        items: updatedState
+                    })
+                    quantityRef.current = userInfo.items[existingItemIndex].quantity + 1;
+                } else {
+                    setUserInfo({
+                        ...userInfo, items: [
+                            ...(userInfo.items || []), {value: item.value, link: item.link, name: item.name, quantity: 1}
+                        ]
+                    });
+                    quantityRef.current = 1;
+                }
+            } else {
+                setCanceled(false);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
         <button 
-            className=' bg-gray-800 
-                        text-white text-xl
-                        rounded-3xl 
-                        w-36 h-11  
-                        hover:opacity-50 ease-in-out duration-200'
+            className=
+            ' bg-gray-800 text-white text-xl rounded-3xl w-36 h-11 hover:opacity-50 ease-in-out duration-200'
                 
             onClick={handleClick}
         >
