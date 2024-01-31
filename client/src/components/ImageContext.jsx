1c72eea61a2820ce2adaab21a7e6a6f7c8bff0f1
Logan Dietel <logandietel1@gmail.com>
2024-01-30 22:39:11 -0800

    Remove useState from ImageProvider component

--- client/src/components/ImageContext.jsx
+++ client/src/components/ImageContext.jsx
@@ -1,15 +1,15 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useContext } from "react";
 
 const ImageContext = createContext();
 
 export function ImageProvider({ children }) {
-    const [imageGroups, setImageGroups] = useState({
+    const imageGroups = {
         home: ['../../public/images/home-imgs/home1.jpg', '../../public/images/home-imgs/home2.jpg', '../../public/images/home-imgs/home3.jpg'],
         hats: [
-               {link: '../../public/images/hat-imgs/hat1.jpg', value: '$54.99', name: 'Hat One', quantity: 0, id: 0}, 
-               {link: '../../public/images/hat-imgs/hat2.jpg', value: '$39.99', name: 'Hat Two', quantity: 0, id: 1},
-               {link: '../../public/images/hat-imgs/hat3.jpg', value: '$34.95', name: 'Hat Three', quantity: 0, id: 2},
-               {link: '../../public/images/hat-imgs/hat4.jpg', value: '$24.99', name: 'Hat Four', quantity: 0, id: 3},
+            {link: '../../public/images/hat-imgs/hat1.jpg', value: '$54.99', name: 'Hat One', quantity: 0, id: 0}, 
+            {link: '../../public/images/hat-imgs/hat2.jpg', value: '$39.99', name: 'Hat Two', quantity: 0, id: 1},
+            {link: '../../public/images/hat-imgs/hat3.jpg', value: '$34.95', name: 'Hat Three', quantity: 0, id: 2},
+            {link: '../../public/images/hat-imgs/hat4.jpg', value: '$24.99', name: 'Hat Four', quantity: 0, id: 3},
         ],
         sweatshirts: [
             {link: '../../public/images/sweatshirt-imgs/sweat1.jpg', value: '$23.99', name: 'Sweatshirt One', quantity: 0, id: 0}, 
@@ -35,7 +35,7 @@ export function ImageProvider({ children }) {
             {link: '../../public/images/shoes-imgs/shoes3.jpg', value: '$38.99', name: 'Shoes Three', quantity: 0, id: 2},
             {link: '../../public/images/shoes-imgs/shoes4.jpg', value: '$62.99', name: 'Shoes Four', quantity: 0, id: 3},
         ],
-    })
+    }
 
     const allClothes = [
         ...imageGroups.hats,
