c5480185362b428e734b77625ed832b7cb7c0f3e
Logan Dietel <logandietel1@gmail.com>
2024-01-30 19:53:26 -0800

    Refactor ProductSectionButton if else statement and remove async from function

--- client/src/components/ProductSectionButton.jsx
+++ client/src/components/ProductSectionButton.jsx
@@ -1,46 +1,38 @@
-import { useEffect, useRef } from "react";
+export default function ProductSectionButton({ item, userInfo, setUserInfo, signedIn, setCanceled }) {
 
-export default function Button({ item, userInfo, setUserInfo, signedIn, setCanceled }) {
+    const handleClick = () => {
 
-    const handleClick = async () => {
-        try {
-            if (signedIn) {
-                const existingItemIndex = userInfo.items?.findIndex(bagItem => {
-                    return bagItem.name === item.name;
+        if (signedIn) {
+            const existingItemIndex = userInfo.items?.findIndex(bagItem => {
+                return bagItem.name === item.name;
+            });
+
+            if (existingItemIndex !== -1 && existingItemIndex !== undefined) {
+                let updatedState = [...userInfo.items];
+                updatedState[existingItemIndex] = {
+                    ...item, 
+                    quantity: userInfo.items[existingItemIndex].quantity + 1
+                };
+
+                setUserInfo({
+                    ...userInfo,
+                    items: updatedState
                 });
-                console.log(existingItemIndex);
-
-                if (existingItemIndex !== -1 && existingItemIndex !== undefined) {
-                    let updatedState = [...userInfo.items];
-                    updatedState[existingItemIndex] = {
-                        ...item, 
-                        quantity: userInfo.items[existingItemIndex].quantity + 1
-                    };
-
-                    setUserInfo({
-                        ...userInfo,
-                        items: updatedState
-                    })
-                } else {
-                    setUserInfo({
-                        ...userInfo, items: [
-                            ...(userInfo.items || []), {value: item.value, link: item.link, name: item.name, quantity: 1}
-                        ]
-                    });
-                }
             } else {
-                setCanceled(false);
+                setUserInfo({
+                    ...userInfo, items: [
+                        ...(userInfo.items || []), {value: item.value, link: item.link, name: item.name, quantity: 1}
+                    ]
+                });
             }
-        } catch (error) {
-            console.log(error);
+        } else {
+            setCanceled(false);
         }
     }
 
     return (
         <button 
-            className=
-            ' bg-gray-800 text-white text-xl rounded-3xl w-36 h-11 opacity-100 hover:opacity-50 ease-in-out duration-200'
-                
+            className=' bg-gray-800 text-white text-xl rounded-3xl w-36 h-11 opacity-100 hover:opacity-50 ease-in-out duration-200'
             onClick={handleClick}
         >
             Add to bag
