4ae4cbb941573bae63f8746c251f96be3bda98c2
Logan Dietel <logandietel1@gmail.com>
2024-02-03 02:50:07 -0800

    Write unit tests for SignUp component

--- client/tests/App.test.jsx
+++ client/tests/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, test, vi } from 'vitest';
 import App from '../src/App';
 import { itemToBag } from '../src/utils';
