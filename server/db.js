4d2b04a373bd064817d9dfa02629f9d70f147214
Logan Dietel <logandietel1@gmail.com>
2024-02-15 14:22:42 -0800

    Add packages to server and scripts to parent directory package.json

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
+
 const pool = new Pool({
-    user: 'logandietel',
-    password: 'password',
-    host: 'localhost',
-    port: 5432,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
     database: 'ecommerce_site'
 })
 
