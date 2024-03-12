067d3498caa4ae63e94f079e7f988e4491f9c465
Logan Dietel <logandietel1@gmail.com>
2024-03-12 15:58:49 -0700

    Create RightDashboardItems component to house right dash items logic

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ import { UserContext } from "./UserContext";
 import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 import {  FaBars, FaTimes } from 'react-icons/fa';
 import LeftDashboardItems from "./LeftDashboardItems";
+import RightDashboardItems from "./RightDashboardItems";
 
 export default function Dashboard () {
     const { setType, signedIn, setSignedIn, setCanceled, userInfo, setUserInfo } = useContext(UserContext);
@@ -46,50 +47,42 @@ export default function Dashboard () {
                 <button value={null} onClick={(e) => handleTypeClick(e)} className='col-start-2 font-pacifico text-4xl'>Lodi NW</button>
 
                 {/*hamburger menu */}
-                <div onClick={() => setShowNav(!showNav)} className="justify-self-end custom-md:hidden z-40">
+                <div onClick={() => setShowNav(!showNav)} className="relative justify-self-end custom-md:hidden z-40 mr-4">
                     {!showNav ? <FaBars size={20} /> : <FaTimes size={20} />}
+                    {(totalItems > 0 && !showNav) && 
+                        <div 
+                            data-testid='itemQuantity' 
+                            className='absolute flex justify-center items-center w-[22px] h-[22px] text-sm bg-red-500 rounded-[50%] top-[-13px] right-[-13px]'
+                        >
+                            {totalItems}
+                        </div>
+                    }
                 </div>
+                
+                <div className={!showNav ? 'hidden' : 'fixed top-0 left-0 w-full h-screen bg-black flex justify-center items-center text-4xl opacity-90 text-white font-pacifico'}>
+                    <div className="flex flex-col gap-16">
+                        <LeftDashboardItems classes={'flex flex-col gap-6'} />
 
-                <LeftDashboardItems classes={!showNav ? 'hidden' : 'fixed top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center text-4xl opacity-90 text-white gap-12 font-pacifico'} />
+                        <RightDashboardItems 
+                            handleBagClick={handleBagClick} 
+                            handleSignOutClick={handleSignOutClick} 
+                            setCanceled={setCanceled} 
+                            totalItems={totalItems} 
+                            signedIn={signedIn} 
+                            classes={'flex flex-col gap-6'} 
+                        />
+                    </div>
+                </div>
 
                 {/* right items */}
-                <div className='justify-self-end hidden custom-md:flex text-sm gap-x-3 gap-y-3'>
-                    {!signedIn ? (
-                        <button 
-                        onClick={() => setCanceled(false)} 
-                        value='signIn' 
-                        className=''
-                        >
-                            Sign in
-                        </button>
-                    ) : (
-                        <button 
-                        className='text-left md:text-center' 
-                        onClick={handleSignOutClick}
-                        >
-                            Sign Out
-                        </button>
-                    )}
-
-                    <button 
-                        data-testid='bag'
-                        onClick={() => handleBagClick()} 
-                        className='relative w-8 md:w-auto'
-                        >
-                        <svg className="w-8 h-8 white dark:white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 9V4a3 3 0 0 0-6 0v5m9.92 10H2.08a1 1 0 0 1-1-1.077L2 6h14l.917 11.923A1 1 0 0 1 15.92 19Z"/>
-                        </svg>
-
-                        {totalItems > 0 && 
-                            <div 
-                            data-testid='itemQuantity' 
-                            className='absolute flex justify-center items-center w-[22px] h-[22px] text-sm bg-red-500 rounded-[50%] top-0 right-[-5px]'
-                            >
-                                {totalItems}
-                            </div>
-                        }
-                    </button>    
-                </div>
+                <RightDashboardItems 
+                    handleBagClick={handleBagClick} 
+                    handleSignOutClick={handleSignOutClick} 
+                    setCanceled={setCanceled} 
+                    totalItems={totalItems} 
+                    signedIn={signedIn} 
+                    classes={'justify-self-end hidden custom-md:flex text-sm gap-x-3 gap-y-3'} 
+                />
             </nav>
 
     )
---
+++ client/src/components/RightDashboardItems.jsx
@@ -0,0 +1,44 @@
+
+const RightDashboardItems = ({ classes, signedIn, handleSignOutClick, handleBagClick, setCanceled, totalItems }) => {
+  return (
+    <div className={classes}>
+        {!signedIn ? (
+            <button 
+                onClick={() => setCanceled(false)} 
+                value='signIn'
+                className="text-left" 
+            >
+                Sign in
+            </button>
+        ) : (
+            <button 
+                className='text-left' 
+                onClick={handleSignOutClick}
+            >
+                Sign Out
+            </button>
+        )}
+
+        <button 
+            data-testid='bag'
+            onClick={() => handleBagClick()} 
+            className='relative w-8'
+        >
+            <svg className="w-8 h-8 white dark:white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 9V4a3 3 0 0 0-6 0v5m9.92 10H2.08a1 1 0 0 1-1-1.077L2 6h14l.917 11.923A1 1 0 0 1 15.92 19Z"/>
+            </svg>
+
+            {totalItems > 0 && 
+                <div 
+                data-testid='itemQuantity' 
+                className='absolute flex justify-center items-center w-[22px] h-[22px] text-sm bg-red-500 rounded-[50%] top-0 right-[-5px]'
+                >
+                    {totalItems}
+                </div>
+            }
+        </button>    
+    </div>
+  )
+}
+
+export default RightDashboardItems
