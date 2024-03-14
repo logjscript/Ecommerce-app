4baf3667385005943624c035d45d38e8b5d33c14
Logan Dietel <logandietel1@gmail.com>
2024-03-14 02:53:38 -0700

    Change item descriptions and card sizing

--- client/src/components/ProductSection.jsx
+++ client/src/components/ProductSection.jsx
@@ -17,14 +17,14 @@ export default function ProductSection() {
                 className="grid grid-cols-[repeat(auto-fit,300px)] justify-evenly items-center gap-11 pt-0 p-8"
                 >
                 {imageGroups[type].map((image) => (
-                    <div key={image.id} className="grid text-center gap-y-4 p-4 bg-gray-100 shadow-lg shadow-gray-400 rounded-3xl">
+                    <div key={image.id} className="grid text-center gap-y-4 p-4 h-[575px] bg-gray-100 shadow-lg shadow-gray-400 rounded-3xl">
                         <div className="flex w-full h-72">
                             <img src={image.link} alt="/" className="place-self-center h-full w-full object-cover rounded-xl shadow-md shadow-gray-400" />
                         </div>
 
                         <div className="text-2xl">{image.name}</div>
 
-                        <p>{image.name} is both a stylish choice and a comfortable fit, making this one of our most loved products</p>
+                        <p>{image.description}</p>
 
                         <div className="text-base text-gray-800 opacity-70">{image.value}</div>
 
